7b0e85e2f144e7d426799169669367fd3512ad7d
danthestar <daniel.matasic@yahoo.com>
2021-04-20 12:20:32 +0000

    adding collision detection

---
+++ Znake/index.js
@@ -0,0 +1,94 @@
+const canvas = document.getElementById('game');
+const rtw = canvas.getContext('2d');
+
+// by changing the number (higher = faster) refreshing time of game
+let speed = 2;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+let foodX = 5;
+let foodY = 5;
+
+let xSpeed = 0;
+let ySpeed = 0;
+
+// game loop sequence's
+function drawZnakeGame(){
+    resetScreen();
+    changeZnakePosition();
+
+    checkFoodCollision();
+    drawFood();
+    drawZnake();
+    setTimeout(drawZnakeGame, 1000/ speed);
+}
+
+function resetScreen(){
+    rtw.fillStyle = 'black';
+    rtw.fillRect(0,0,canvas.width,canvas.height);
+}
+// snake on display :D
+function drawZnake(){
+    rtw.fillStyle = 'white';
+    rtw.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeZnakePosition(){
+    headX = headX + xSpeed;
+    headY = headY + ySpeed;
+}
+
+// food on display 
+function drawFood(){
+    rtw.fillStyle = "orange";
+    rtw.fillRect(foodX * tileCount, foodY * tileCount, tileSize, tileSize)
+}
+
+// food disapears on collison and apears randomly in tilecount area :D
+function checkFoodCollision(){
+    if(foodX === headX && foodY == headY){
+        foodX = Math.floor(Math.random() * tileCount);
+        foodY = Math.floor(Math.random() * tileCount);
+    }
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event){
+    // code for key "up" it 38 
+    if(event.keyCode == 38){ 
+        if(ySpeed == 1)
+            return; // blocking to go back aginst the movment
+        ySpeed = -1;
+        xSpeed = 0;
+    }
+    // code for key "down" it 40
+    if(event.keyCode == 40){
+        if(ySpeed == -1)
+            return;
+        ySpeed = 1;
+        xSpeed = 0;
+    }
+    // code for key "left" it 37
+    if(event.keyCode == 37){
+        if(xSpeed == 1)
+            return;
+        ySpeed = 0;
+        xSpeed = -1;
+    }
+    // code for key "right" it 39
+    if(event.keyCode == 39){
+        if(xSpeed == -1)
+            return;
+        ySpeed = 0;
+        xSpeed = 1;
+    }
+
+}
+
+drawZnakeGame();
+
+   
