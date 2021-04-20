a3ec778135e0a2b155d731ac3c6e41a15ce94da5
danthestar <daniel.matasic@yahoo.com>
2021-04-20 14:19:36 +0000

    fixing typo mistakes(that cost me huge portion of time)and adding znake eating/geting longer part

--- Znake/index.js
+++ Znake/index.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById('game');
 const rtw = canvas.getContext('2d');
 
+class ZnakePart{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 // by changing the number (higher = faster) refreshing time of game
 let speed = 2;
 
@@ -9,21 +16,37 @@ let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
 
+const znakeParts = [];
+let tailLength = 0;  // chaniging starting SIZE of znakes body (higer number = longer body)
+
 let foodX = 5;
 let foodY = 5;
 
 let xSpeed = 0;
 let ySpeed = 0;
 
+let result = 0;
+
 // game loop sequence's
 function drawZnakeGame(){
-    resetScreen();
     changeZnakePosition();
+    resetScreen();
+    
 
     checkFoodCollision();
     drawFood();
     drawZnake();
+
+    drawResult();
+
     setTimeout(drawZnakeGame, 1000/ speed);
+}
+
+// score result display window
+function drawResult(){
+    rtw.fillStyle = 'white';
+    rtw.font = "10px Verdana"
+    rtw.fillText("Score " + result, canvas.width - 50, 10)
 }
 
 function resetScreen(){
@@ -34,6 +57,17 @@ function resetScreen(){
 function drawZnake(){
     rtw.fillStyle = 'white';
     rtw.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
+    rtw.fillStyle = 'green';
+    for(let i = 0; i < znakeParts.length; i++){
+        let part = znakeParts[i];
+        rtw.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+
+    znakeParts.push(new ZnakePart(headX, headY)); // longggg znake body
+    while(znakeParts.length > tailLength){  // by changing if statemant to while allows us more option in future!
+        znakeParts.shift(); // body of znake remains in corect lenght on "if" or improved "while" version of statment!! :D
+    }
 }
 
 function changeZnakePosition(){
@@ -52,6 +86,8 @@ function checkFoodCollision(){
     if(foodX === headX && foodY == headY){
         foodX = Math.floor(Math.random() * tileCount);
         foodY = Math.floor(Math.random() * tileCount);
+        tailLength++;
+        result++;  // chainging score each time we eat "food"
     }
 }
 
@@ -90,5 +126,3 @@ function keyDown(event){
 }
 
 drawZnakeGame();
-
-   
